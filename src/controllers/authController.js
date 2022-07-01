30f5c1b29564088b1bc63d5617aff1b150ca4412
Daniele <daniele.maria.silvadm@gmail.com>
2022-07-01 12:54:44 -0300

    Dani

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,51 @@
 const UserSchema = require('../models/userSchema');
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const SECRET = process.env.SECRET
 
 const login = (req, res) => {
+
+    try{
+        //userSchema.findOne(filtro é o email do usuario, funçao anonima)
+
+        UserSchema.findOne({ email: req.body.email }, (error, user) => {
+            console.log("USUÁRIO", user)
+            if(!user) {
+                return res.status(401).send({
+                    message: "User não encontrado",
+                    email: `${req.body.email}`
+                })
+            }
+
+        // o que eu tenho: usuario que veio da requisiçao e o usuario encontrado no anco de dados, ambos
+        //os usuarios possuem o mesmo email, agora preciso verificar se eles tambem possuem a mesma senha
+
+        const validPassword = bcrypt.compareSync(req.body.password, user.password)
+        console.log(validPassword)
+
+        if(!validPassword) {
+            return res.status(401).send({
+                "message": "Login não autorizado"
+            })
+          }
+
+          //jwt.sign(nome do usuario, segredo)
+          const token = jwt.sign({name: user.name}, SECRET)
+          console.log("TOKEN CRIADO", token)
+
+          res.status(200).send({
+            "message": "Login autorizado",
+            token
+          })
+    
+        })
+
+    }catch(e) { 
+        console.error(e)
+    }
+
+       
+          
     
 };
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,71 @@
 const UserSchema = require("../models/userSchema");
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
+const SECRET = process.env.SECRET
 
 const getAll = async (req, res) => {
+  const authHeader = req.get('authorization')
+  const token = authHeader.split(' ')[1]
+ 
+
+  if (!token) {
+    return res.status(401).send("Erro no header")
+  }
+
+  jwt.verify(token, SECRET, (err) => {
+    if(err) {
+      return res.status(401).send("Não autorizado")
+    }
+  })
+    
   UserSchema.find(function (err, users) {
     if(err) {
       res.status(500).send({ message: err.message })
     }
       res.status(200).send(users)
   }) 
-};
+}
+
+const deleteId = async (req, res) => {
+  try {
+    const userId = req.params.id
+    const deleteUser = await userSchema.findByIdAndDelete(userId) 
+
+    res.status(200).json ([
+      "message": "User deletado com sucesso",
+      "usuário": deleteUser
+    ])
+
+
+  }catch(error){
+    console.error(error)
+
+  }
+
+}
+
+
+const createUser = async (req, res) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+  req.body.password = hashedPassword
+  
+  try {
+    const newUser = new UserSchema(req.body)
+    const saveUser = await newUser.save()
+
+    res.status(201).send({
+      "message": "User criado com sucesso",
+      saveUser
+    })
+
+  } catch(error) {
+    console.error(error)
+
+  }
+
+}
 
 module.exports = {
-  getAll
+  getAll,
+  createUser
 };
